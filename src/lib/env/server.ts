cc322eb8b07ef126bced2f2d8637b301ed14ebc1
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2025-10-23 00:07:42 +0100

    refactor: update server environment configuration for clarity
    - Removed unused GOOGLE_CLOUD_PROJECT_ID and GOOGLE_APPLICATION_CREDENTIALS_JSON
      variables from server environment configuration.
    - Added NODE_ENV variable with default value set to "development" and allowed
      values for better environment management.

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -9,12 +9,10 @@ export const serverEnv = createEnv({
     DATABASE_URL: z.url(),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
-    GOOGLE_CLOUD_PROJECT_ID: z.string().min(1),
-    GOOGLE_APPLICATION_CREDENTIALS_JSON: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
-    // NODE_ENV: z
-    //   .enum(["development", "test", "production"])
-    //   .default("development"),
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
   },
   //  For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: process.env,
