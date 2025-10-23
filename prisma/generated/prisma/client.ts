f6dc5bb8b3b35e098b29ee63f7a1bdf45a1c8876
Abdulmujeeb Ahmad <abdulmujeebahmad03@gmail.com>
2025-10-23 08:15:36 +0100

    chore(prisma): update engineType to 'client' in schema.prisma

--- prisma/generated/prisma/client.ts
+++ prisma/generated/prisma/client.ts
@@ -13,7 +13,7 @@ import * as path from "node:path";
 import { fileURLToPath } from "node:url";
 globalThis["__dirname"] = path.dirname(fileURLToPath(import.meta.url));
 
-import * as runtime from "@prisma/client/runtime/library";
+import * as runtime from "@prisma/client/runtime/client";
 import * as $Enums from "./enums";
 import * as $Class from "./internal/class";
 import * as Prisma from "./internal/prismaNamespace";
@@ -42,20 +42,6 @@ export type PrismaClient<
     runtime.Types.Extensions.InternalArgs = runtime.Types.Extensions.DefaultArgs,
 > = $Class.PrismaClient<LogOpts, OmitOpts, ExtArgs>;
 export { Prisma };
-
-// file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-darwin-arm64.dylib.node");
-path.join(
-  process.cwd(),
-  "prisma/generated/prisma/libquery_engine-darwin-arm64.dylib.node"
-);
-
-// file annotations for bundling tools to include these files
-path.join(__dirname, "libquery_engine-rhel-openssl-3.0.x.so.node");
-path.join(
-  process.cwd(),
-  "prisma/generated/prisma/libquery_engine-rhel-openssl-3.0.x.so.node"
-);
 
 /**
  * Model User
