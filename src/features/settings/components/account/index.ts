cf090008030bdf117f78f8a6b54bc075a65586c9
Joshua Obafemi <obafemijoshua2020@gmail.com>
2026-06-02 13:34:23 +0100

    chore: remove legacy settings code, test setup, and outdated docs
    Remove deprecated account settings components, integration-related settings components,
    obsolete test setup file, and outdated project documentation that is no longer
    used or maintained in the current codebase.

--- src/features/settings/components/account/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Account } from "./account";
-export { default as Branding } from "./branding";
-export { default as CompanyInfo } from "./company-info";
-export { default as SenderVerification } from "./sender-verification";
-export { default as TeamMembers } from "./team-members";
